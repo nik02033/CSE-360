22d2f1a5076d14421f2da8ba666570605add5968
nik02033 <nkanodi1@asu.edu>
2024-03-20 15:37:50 -0700

    Categorize classes

---
+++ Project/basic.java
@@ -0,0 +1,17 @@
+package Project;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+
+public class basic extends Application
+{
+	public void start(Stage stage) {
+		SignUp obj = new SignUp(stage);
+		obj.start(stage);
+	}
+	
+	 public static void main(String[] args) {
+	        launch(args);
+	 }
+}
